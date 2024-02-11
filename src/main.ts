0a72a5b8c12f2aa7074d037926ce790667d5e830
Borisov Maxim <voodivood@gmail.com>
2024-02-11 19:04:48 +0300

    drop origin

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     const PORT = process.env.PORT || 5001;
     app.enableCors({
-        origin: ['http://localhost:3000', 'http://localhost:3001', 'https://holod-vdom.store'],
+        origin: false,
+        // ['http://localhost:3000', 'http://localhost:3001', 'https://holod-vdom.store'],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         preflightContinue: false,
         optionsSuccessStatus: 204,
